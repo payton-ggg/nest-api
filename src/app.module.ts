ff4824b6a81178b55b141471fe4dea3fc102d452
Platon <platon20081019@gmail.com>
2025-07-05 12:43:56 +0300

    aSSDAA

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, AuthModule, BookmarkModule],
+  imports: [UserModule, AuthModule, BookmarkModule, PrismaModule],
 })
 export class AppModule {}
